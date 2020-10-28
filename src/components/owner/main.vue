4eba5344501ab70b219051a94c6ead92b6794bac
philipretl <philipreaver@gmail.com>
2020-10-28 00:43:30 -0500

    initial desing finished

---
+++ src/components/owner/main.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="main-owners">
+    <div class="max-w-full min-h-full rounded-xl  overflow-hidden shadow-lg bg-white my-2 mx-2">
+      <div class="flex flex-row-reverse pr-4 py-4">
+        <button class="bg-gray-900 hover:bg-yellow-400 text-white font-bold py-2 px-4 rounded">
+          Agregar propietario
+        </button>
+      </div>
+
+      <div class="px-6 pt-4 pb-8">
+        <div class="text-3xl text-gray-900">
+          List de Propietarios
+        </div>
+        <hr>
+        <table class="table-fixed w-full mt-10">
+          <thead>
+          <tr >
+            <th class="w-1/2 px-2 py-2 text-left">Nombre completo</th>
+            <th class="w-1/2 px-2 py-2 text-left">dni</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr>
+            <td class="border px-4 py-2">Intro to CSS</td>
+            <td class="border px-4 py-2">Adam</td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+
+    </div>
+  </div>
+</template>
---
+++ src/components/vehicle/main.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="main-vehicles">
+    <div class="max-w-full min-h-full h-full rounded  overflow-hidden shadow-lg bg-white mt-2 mx-2">
+      <div class="flex flex-row-reverse pr-4 py-4">
+        <button class="bg-gray-900 hover:bg-yellow-400 text-white font-bold py-2 px-4 rounded">
+          Agregar Vehiculo
+        </button>
+      </div>
+      <div class="px-6 pt-4 pb-8">
+        <div class="text-3xl text-gray-900">
+          List de Vehiculos
+        </div>
+        <hr>
+        <table class="table-fixed w-full mt-10">
+          <thead>
+          <tr >
+            <th class="w-1/4 px-2 py-2 text-left">Placa</th>
+            <th class="w-1/2 px-2 py-2 text-left">Marca</th>
+            <th class="w-1/2 px-2 py-2 text-left">Modelo</th>
+            <th class="w-1/2 px-2 py-2 text-left">Tipo</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr>
+            <td class="border px-4 py-2">PlX 125</td>
+            <td class="border px-4 py-2">Renault</td>
+            <td class="border px-4 py-2">Sandero</td>
+            <td class="border px-4 py-2">Carro</td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default{
+
+}
+</script>
